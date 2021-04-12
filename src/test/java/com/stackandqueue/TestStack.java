07903d3ff574a2f5c8439e4dcd43bbd85a685c7d
dinnu247 <dineshkumar.icon@gmail.com>
2021-04-12 08:16:06 +0530

    ADDED: pop method to Delete Last Added Key

--- src/test/java/com/stackandqueue/TestStack.java
+++ src/test/java/com/stackandqueue/TestStack.java
@@ -19,6 +19,27 @@ public class TestStack {
         stack.printStack();
 
         INode<Integer> peak = stack.peak();
+
         Assertions.assertEquals(thirdNode, peak);
+    }
+
+    @Test
+    public void given3IntegersIntoStackWhenPoppedShouldHaveLastAddedRemoved(){
+        Node<Integer> firstNode = new Node<>(70);
+        Node<Integer> secondNode = new Node<>(30);
+        Node<Integer> thirdNode = new Node<>(56);
+
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(firstNode);
+        stack.push(secondNode);
+        stack.push(thirdNode);
+
+        INode<Integer> pop = stack.pop();
+
+        stack.printStack();
+
+
+        Assertions.assertEquals(thirdNode, pop);
     }
 }
