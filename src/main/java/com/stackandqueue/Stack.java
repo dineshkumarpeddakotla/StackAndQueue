498d1993cd966f8c176ff710e4dfce34d06833cc
dinnu247 <dineshkumar.icon@gmail.com>
2021-04-12 08:02:28 +0530

    ADDED: push and peak method When Added the Keys Should be Last

--- src/main/java/com/stackandqueue/Stack.java
+++ src/main/java/com/stackandqueue/Stack.java
@@ -1,4 +1,17 @@
 package com.stackandqueue;
 
-public class Stack {
+public class Stack<K> {
+    private LinkedList<K> linkedList = new LinkedList<>();
+
+    public void push(INode<K> node) {
+        linkedList.add(node);
+    }
+
+    public INode<K> peak() {
+        return linkedList.head;
+    }
+
+    public void printStack() {
+        linkedList.printNode();
+    }
 }
